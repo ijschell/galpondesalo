62c709364b4642dec2e897709c45ee618e901de8
Jonathan <jonathans@takeoffmedia.com>
2017-11-17 22:32:48 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>NP CSS Project</title>
+
+    <!-- Bootstrap v4.0 -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/vendor/bootstrap/css/bootstrap.min.css">
+    <!-- Generated CSS -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/assets/css/master.css">
+  </head>
+  <body>
+    <section>
+      <div class="sub-section">
+        <h1>Título H1</h1>
+        <p>Párrafo con <a href="#">link</a></p>
+        <p class="big-text">Párrafo big text</p>
+        <p class="small-text">Párrafo small text</p>
+      </div>
+      <div class="sub-section">
+        <h2>Título H2</h2>
+      </div>
+      <div class="sub-section">
+        <h3>Título H3</h3>
+      </div>
+      <div class="sub-section">
+        <h4>Título H4</h4>
+      </div>
+      <div class="sub-section">
+        <h5>Título H5</h5>
+      </div>
+    </section>
+
+    <!-- JavaScript includes -->
+    <script type="text/javascript" src="<?php echo get_template_directory_uri()?>/vendor/jquery/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri()?>/vendor/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
